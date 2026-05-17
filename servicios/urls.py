3a93a7522664c5207f3d3da9f845c56ca97f27a7
osogobio2004 <rominaosogobio2004@gmail.com>
2026-05-16 18:32:35 -0600

    login, registro, about y contact

--- servicios/urls.py
+++ servicios/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.vista_login, name='login'),
+    path('registro/', views.vista_registro, name='registro'),
+    path('about/', views.about, name='nosotros'),   # Ruta de Nosotros
+    path('contact/', views.contact, name='contacto'), # Ruta de Contacto
 ]
--- servicios/views.py
+++ servicios/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from .models import PerfilTecnico
 
+# Vista de la página de inicio (ya la teníamos)
 def index(request):
-    tecnicos = PerfilTecnico.objects.all()
-    return render(request, 'servicios/home.html', {'tecnicos': tecnicos})
+    return render(request, 'servicios/home.html')
+
+# Nueva vista temporal para ver el Login
+def vista_login(request):
+    return render(request, 'registration/login.html')
+
+# Nueva vista temporal para ver el Registro
+def vista_registro(request):
+    return render(request, 'registration/registro.html')
+
+def about(request):
+    return render(request, 'servicios/about.html')
+
+def contact(request):
+    return render(request, 'servicios/contact.html')
