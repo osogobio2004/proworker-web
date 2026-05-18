c77af5444392e8d153ea12c3eb0bca164a9cc6b9
osogobio2004 <rominaosogobio2004@gmail.com>
2026-05-17 19:13:23 -0600

    Preparado para el despliegue en Render

--- proworker_core/settings.py
+++ proworker_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w8ky*vg@8-=us2!z7-srbmevor80n-p7!$wa^1d(h(lx8&ipf1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,3 +129,7 @@ import os
 # Archivos subidos por los usuarios (Imágenes)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Opcional: optimización de almacenamiento para WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
